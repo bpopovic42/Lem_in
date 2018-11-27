14483f77cd6af646368bb7870745d422c060ea9e
Boris POPOVIC <bopopovi@student.42.fr>
2018-11-27 19:43:06 +0100

    [ADD] Implemented new functions get_ants and get_rooms in main, added an exit function

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 22:56:40 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/11/20 14:29:37 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/11/27 19:31:20 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,28 @@ static void		free_data(t_graph *graph, char **input)
 	graph->rooms = NULL;
 }
 
+static int		exit_error(const char *msg, t_graph *graph, char **input)
+{
+	free_data(graph, input);
+	ft_putendl(msg);
+	return (-1);
+}
+
 int		main(void)
 {
 	/* Create needed structures */
-	size_t		ants;
-	t_graph		graph;
-	char		**input;
+	unsigned int	ants;
+	t_graph			graph;
+	char			**input;
 
 	ants = 0;
 	init_graph(&graph);
 	/* Check and read input into previously created structures */
 	input = get_input();
+	if (get_ants_nbr(input, &ants) < 0)
+		exit_error("main: invalid ants input", &graph, input);
+	if (get_rooms(input, &graph) < 0)
+		exit_error("main: bad formatted rooms input", &graph, input);
 	/* Apply path-finding algorithm to find shortest paths */
 	/* Apply algorithm to find most efficient paths depending on ants quantity */
 	/* Output solution */
