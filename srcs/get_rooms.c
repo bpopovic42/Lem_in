5c4564d4a93bbbfe22cc9b95a8dac21a7e096f62
Boris POPOVIC <bopopovi@student.42.fr>
2018-11-28 20:54:28 +0100

    [WIP] Implementing recording of rooms's definition into a vector

--- srcs/get_rooms.c
+++ srcs/get_rooms.c
@@ -6,19 +6,35 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 18:41:55 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/11/27 19:40:39 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/11/28 19:51:51 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include "ft_printf.h"
 
+static void		record_room(t_graph *graph, char **room, char *comment)
+{
+	t_room	*new_room;
+	t_pos	*coord;
+
+	if (!room)
+		ft_putstr("no room");
+	new_room = malloc(sizeof(*new_room));
+	coord = malloc(sizeof(*coord));
+	coord->x = ft_atoi(room[1]);
+	coord->y = ft_atoi(room[2]);
+	new_room->comment = ft_strdup(comment);
+	new_room->name = ft_strdup(room[0]);
+	new_room->links = NULL;
+	new_room->position = coord;
+	ft_vector_append(graph->rooms, new_room);
+}
+
 int		get_rooms(char **input, t_graph *graph)
 {
 	int		i;
 	char	*comment_ptr;
-	(void)input;
-	(void)graph;
 
 	i = 1;
 	comment_ptr = NULL;
@@ -28,17 +44,13 @@ int		get_rooms(char **input, t_graph *graph)
 	{
 		/* If line is a comment */
 		/* Save a pointer to it */
-		ft_putnbr(ft_count_words(input[i], WSPCS));
 		if (*input[i] == '#')
-		{
-			ft_printf("%s : %s\n", "Comment", input[i]);
 			comment_ptr = input[i];
-		}
-		/* Else line must be a room definition */
+		/* Else line must be a room definition or end of array*/
 		/* Call input recording function */
-		else
+		else if (input[i])
 		{
-			ft_printf("%s : %s\n", "Room", input[i]);
+			record_room(graph, ft_strsplit(input[i], WSPCS), comment_ptr);
 		}
 		i++;
 	}
