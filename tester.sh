e80784dc63816893eb8e2bbc0d424ced151d5bd5
bpopov <byurinov1@gmail.com>
2018-12-23 01:32:01 +0100

    [MOD][ADD] Improved tester, added leaks checking support (./tester.sh leaks)

--- tester.sh
+++ tester.sh
@@ -1,11 +1,51 @@
 #!/bin/bash
 
 TEST_OUT_DIR="test_output"
+SEPARATOR="########################################"
 MAP_DIR="map_samples"
 EXE="lem-in"
+TMP=""
+LEAKS=$([[ $1 = "leaks" ]] && echo "1" || echo "0")
 
-if [ ! -e $EXE ]; then
-	make
+function overwrite_if_file_exists () {
+	if [ -e "$TEST_OUT_DIR/$outputFile.txt" ]; then
+		read -p "File $1.txt already exists, overwrite ?" yn
+		case $yn in
+			[Yy]* ) printf "" > $TEST_OUT_DIR/$1.txt;;
+			[Nn]* ) exit;;
+		esac
+	fi;
+}
+
+function check_leaks () {
+	file="$1"
+	output="$2"
+	if [ $LEAKS == 1 ]; then
+		vg=$(valgrind --leak-check=full ./$EXE < $file 2>&1)
+		vg=$(echo "$vg" | grep -A1 "definitely lost:" | cut -c 14-)
+		has_leaks=$(printf "$vg" | sed "s/[^0-9]//g")
+		has_leaks=$(echo $has_leaks | sed 's/ //g')
+		if [ ! "$has_leaks" == "0000" ]; then
+			echo -e "LEAKS :\n$vg\n" >> $output
+		fi;
+	fi;
+}
+
+function test_each_file_in_dir () {
+	output="$TEST_OUT_DIR/$2.txt"
+	for file in $1/*; do
+		printf "[ ${file##*/} ] :\n\n" >> $output
+		result=$(eval ./$EXE < $file 2>&1 | cat)
+		echo -e "$result\n" >> $output 2>&1
+		check_leaks $file $output
+		echo -e "$SEPARATOR\n\n" >> $output
+	done;
+}
+
+if [ $LEAKS == 1 ]; then
+	make re DEBUG=-g
+elif [ ! -e $EXE ]; then
+	make DEBUG=-g
 	if [ $? -ne 0 ]; then
 		exit 1
 	fi
@@ -13,18 +53,7 @@ fi
 
 for dir in $MAP_DIR/*; do
 	outputFile="${dir##*/}";
-	if [ -e "$TEST_OUT_DIR/$outputFile.txt" ]; then
-		read -p "File $outputFile.txt already exists, overwrite ?" yn
-		case $yn in
-			[Yy]* ) printf "" > $TEST_OUT_DIR/$outputFile.txt;;
-			[Nn]* ) exit;;
-		esac
-	fi;
-	for file in $dir/*; do
-		echo "[ ${file##*/} ] :" >> $TEST_OUT_DIR/$outputFile.txt
-		result=$(eval ./$EXE < $file 2>&1 | cat)
-		echo $result >> $TEST_OUT_DIR/$outputFile.txt 2>&1
-		printf "***************************************\n" >> $TEST_OUT_DIR/$outputFile.txt
-	done;
+	overwrite_if_file_exists $outputFile
+	test_each_file_in_dir $dir $outputFile
 done
 
