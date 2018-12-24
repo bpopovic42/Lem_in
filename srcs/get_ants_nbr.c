42d39bffb39cc5137115d2d0024b00d07a48be69
bpopov <byurinov1@gmail.com>
2018-12-24 01:55:24 +0100

    [MOD] Simplified file's code and sent ft_is_valid_int to libft

--- srcs/get_ants_nbr.c
+++ srcs/get_ants_nbr.c
@@ -6,42 +6,19 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 17:52:41 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/20 19:40:10 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/12/24 00:48:01 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-static int		ft_is_valid_int(const char *nbr)
-{
-	int i;
-
-	i = 0;
-	while (nbr[i])
-	{
-		if (!ft_isdigit(nbr[i]) && !ft_issign(nbr[i]))
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-static int		strict_atoi(const char *nbr, int *result)
-{
-	if (ft_is_valid_int(nbr))
-	{
-		*result = ft_atoi(nbr);
-		return (0);
-	}
-	return (-1);
-}
-
 static int		get_ants_nbr_if_valid(const char *str, unsigned int *output)
 {
 	int result;
 
-	if (strict_atoi(str, &result) < 0)
-		return (-1);
+	result = 0;
+	if (ft_is_valid_int(str))
+		result = ft_atoi(str);
 	if (result <= 0)
 		return (-1);
 	*output = (unsigned int)result;
