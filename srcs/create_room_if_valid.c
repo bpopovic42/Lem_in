d587a3b78971f7b25a5c206e4f0a68c0d5646953
bpopov <byurinov1@gmail.com>
2018-12-24 01:56:45 +0100

    [MOD] Renamed and refactored file quite heavily, now handles some more error cases

--- srcs/create_room_if_valid.c
+++ srcs/create_room_if_valid.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_room.c                                         :+:      :+:    :+:   */
+/*   create_room_if_valid.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 18:41:55 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/23 21:58:34 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/12/24 01:41:21 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,14 @@ static t_room	*new_room(char *name, char **com, t_links *links, t_pos *coord)
 	return (new_room);
 }
 
-static void		record_room(t_graph *graph, char **room_data, char **comment)
+static t_room	*record_room(t_graph *graph, char **room_data, char **comment)
 {
 	t_pos	coord;
 	t_room	*room;
-	t_hash	*new_hash;
 
 	coord.x = ft_atoi(room_data[1]);
 	coord.y = ft_atoi(room_data[2]);
 	room = new_room(room_data[0], comment, NULL, &coord);
-	new_hash = ft_hashnew(room->name, room, sizeof(*room));
-	ft_hashpush(graph->rooms, new_hash);
 	if (room->comment)
 	{
 		if (!(ft_strcmp(room->comment, "##start")))
@@ -45,47 +42,68 @@ static void		record_room(t_graph *graph, char **room_data, char **comment)
 		else if (!(ft_strcmp(room->comment, "##end")))
 			graph->end = room;
 	}
+	return (room);
 }
 
-int		check_position_conflict(t_graph *graph, int x, int y)
+static int		check_coord(char *x, char *y)
 {
-	int i;
-	t_hash *room_ptr;
+	size_t	i;
+	int		coord_x;
+	int		coord_y;
 
 	i = 0;
-	room_ptr = NULL;
-	if (graph->rooms && graph->rooms->size != 0)
+	if (!ft_is_valid_int(x) || !ft_is_valid_int(y))
+		return (-1);
+	coord_x = ft_atoi(x);
+	coord_y = ft_atoi(y);
+	if (coord_x < 0 || coord_y < 0)
+		return (-1);
+	return (0);
+}
+
+static int		check_conflict(t_graph *graph, t_room *room)
+{
+	t_room	*ptr;
+	size_t	i;
+
+	i = 0;
+	if (!graph->rooms || !graph->rooms->size)
+		return (0);
+	while (i < graph->room_list->size)
 	{
-		while (i < (int)graph->room_list->size)
+		if ((ptr = ft_hashget_data(graph->rooms, graph->room_list->data[i])))
 		{
-			room_ptr = ft_hashget(graph->rooms, graph->room_list->data[i]);
-			if (room_ptr)
-			{
-				t_room *test = room_ptr->data;
-				if (test->pos.x == (size_t)x && test->pos.y == (size_t)y)
-					return (1);
-			}
-			i++;
+			if (!ft_strcmp(room->name, ptr->name))
+				return (1);
+			else if (room->pos.x == ptr->pos.x && room->pos.y == ptr->pos.y)
+				return (1);
 		}
+		i++;
 	}
 	return (0);
 }
 
-int		get_room(char *input, char **comment, t_graph *graph)
+int				create_room_if_valid(char *input, char **cmnt, t_graph *graph)
 {
-	char **tmp;
+	char	**tmp;
+	t_room	*room;
 
 	if (input)
 	{
-		if (ft_strchr(input, '-'))
-			return (-1);
 		tmp = ft_strsplit(input, WSPCS);
-		ft_vector_append(graph->room_list, (void*)ft_strdup(tmp[0]));
-		if (check_position_conflict(graph, ft_atoi(tmp[1]), ft_atoi(tmp[2])))
-			return (-1);
-		record_room(graph, tmp, comment);
+		if (!ft_strchr(tmp[0], '-') && check_coord(tmp[1], tmp[2]) >= 0)
+		{
+			room = record_room(graph, tmp, cmnt);
+			if (!check_conflict(graph, room))
+			{
+				ft_vector_append(graph->room_list, (void*)ft_strdup(tmp[0]));
+				ft_hashpush_data(graph->rooms, room->name, room, sizeof(*room));
+				ft_delarray(tmp);
+				return (0);
+			}
+			free_room(room);
+		}
 		ft_delarray(tmp);
-		return (0);
 	}
 	return (-1);
 }
