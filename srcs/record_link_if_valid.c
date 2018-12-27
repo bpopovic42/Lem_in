ccce14e4c01d29c6e2947eaa6715524f9acee429
Boris POPOVIC <bopopovi@student.42.fr>
2018-12-27 16:09:07 +0100

    [MOD] Refactor

--- srcs/record_link_if_valid.c
+++ srcs/record_link_if_valid.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 20:06:07 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/27 14:14:31 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/12/27 16:09:20 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,14 @@ static int	room_exists(t_graph *graph, char *room)
 	size_t i;
 
 	i = 0;
-	if (graph->size && graph->room_list)
+	if (!graph->size || !graph->room_list)
+		return (0);
+	while (i < graph->size)
 	{
-		while (i < graph->size)
-		{
-			if (!ft_strcmp(graph->room_list->data[i], room))
-				return (1);
-			i++;
-		}
+		if (!ft_strcmp(graph->room_list->data[i], room))
+			return (1);
+		i++;
 	}
-	lemin_perror("Linked room doesn't exist.");
 	return (0);
 }
 
@@ -36,49 +34,38 @@ static int	link_exists(t_graph *graph, char *room_a, char *room_b)
 	t_room	*ptr;
 
 	i = 0;
-	if (!(ptr = ft_hashget_data(graph->rooms, room_a)))
-		return (-1);
-	if (ptr->links && ptr->links->size)
+	ptr = ft_hashget_data(graph->rooms, room_a);
+	if (!ptr->links || !ptr->links->size)
+		return (0);
+	while (i < ptr->links->size)
 	{
-		while (i < ptr->links->size)
-		{
-			if (!ft_strcmp(ptr->links->data[i], room_b))
-			{
-				lemin_perror("Link already exists.");
-				return (1);
-			}
-			i++;
-		}
+		if (!ft_strcmp(ptr->links->data[i], room_b))
+			return (1);
+		i++;
 	}
 	return (0);
 }
 
-int		record_link_if_valid(t_graph *graph, const char *link)
+static int	local_return(char *msg, char **rooms, int return_value)
 {
-	char **rooms;
+	if (msg)
+		lemin_perror(msg);
+	if (rooms)
+		ft_delarray(rooms);
+	return (return_value);
+}
 
-	rooms = NULL;
-	if (link && (rooms = ft_strsplit(link, "-")) == NULL)
-		exit(-1);
+int			record_link_if_valid(t_graph *graph, const char *link)
+{
+	char	**rooms;
+
+	rooms = ft_strsplit(link, "-");
 	if (!ft_strcmp(rooms[0], rooms[1]))
-	{
-		lemin_perror("Room links to itself.");
-		return (-1);
-	}
-	if (room_exists(graph, rooms[0]) && room_exists(graph, rooms[1]))
-	{
-		if (link_exists(graph, rooms[0], rooms[1]))
-		{
-			ft_delarray(rooms);
-			return (-1);
-		}
-		else
-		{
-			record_link(graph, rooms[0], rooms[1]);
-			ft_delarray(rooms);
-			return (0);
-		}
-	}
-	ft_delarray(rooms);
-	return (-1);
+		return (local_return("Room links to itself.", rooms, -1));
+	else if (!room_exists(graph, rooms[0]) || !room_exists(graph, rooms[1]))
+		return (local_return("Linked room doesn't exist.", rooms, -1));
+	else if (link_exists(graph, rooms[0], rooms[1]))
+		return (local_return("Link already exists.", rooms, -1));
+	record_link(graph, rooms[0], rooms[1]);
+	return (local_return(NULL, rooms, 0));
 }
