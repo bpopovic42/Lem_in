4995a65fa4f64e5c2d61234afa7c3e180d0d7d2b
Boris POPOVIC <bopopovi@student.42.fr>
2018-12-28 20:10:11 +0100

    [MOD] Refactor

--- srcs/parse_input.c
+++ srcs/parse_input.c
@@ -6,63 +6,47 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/14 16:59:13 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/28 18:33:41 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/12/28 20:04:45 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include "get_next_line.h"
 
-int				is_comment(char *line)
+static int		local_exit(int retval, char *msg, char *cmd)
 {
-	return (*line == '#' && *(line + 1) != '#');
-}
-
-int				is_command(char *line)
-{
-	return (*line == '#' && *(line + 1) == '#');
-}
-
-static void		get_command(char *line, char **cmd)
-{
-	if (*cmd)
-		ft_strdel(cmd);
-	*cmd = ft_strdup(line);
-}
-
-static int		get_ants(char *input, int *ants)
-{
-	if (!ft_is_valid_int(input))
-	{
-		lemin_perror("Invalid ants integer representation.");
-		return (-1);
-	}
-	if (*input == '-' || (*ants = ft_atoi(input)) <= 0)
-	{
-		lemin_perror("Invalid ants integer value.");
-		return (-1);
-	}
-	return (0);
+	if (cmd)
+		ft_strdel(&cmd);
+	if (msg)
+		lemin_perror(msg);
+	return (retval);
 }
 
 static int		data_missing(t_graph *graph)
 {
 	if (!graph->nbr_of_rooms)
-	{
-		lemin_perror("No rooms provided.");
-		return (1);
-	}
+		return (local_exit(1, "No rooms provided.", NULL));
 	else if (!graph->nbr_of_links)
-	{
-		lemin_perror("No links provided.");
-		return (1);
-	}
+		return (local_exit(1, "No links provided.", NULL));
 	else if (!graph->start || !graph->end)
-	{
-		lemin_perror("No start and/or end room.");
-		return (1);
-	}
+		return (local_exit(1, "Missing start and/or end room.", NULL));
 	return (0);
+}
+
+static int		get_ants(char *input, int *ants)
+{
+	if (!ft_is_valid_int(input))
+		return (local_exit(-1, "Invalid ants integer representation.", NULL));
+	if (*input == '-' || (*ants = ft_atoi(input)) <= 0)
+		return (local_exit(-1, "Invalid ants integer value.", NULL));
+	return (0);
+}
+
+static void		get_cmd(char *line, char **cmd)
+{
+	if (*cmd)
+		ft_strdel(cmd);
+	*cmd = ft_strdup(line);
 }
 
 int				parse_input(int *ants, t_graph *graph, char **file)
@@ -71,23 +55,24 @@ int				parse_input(int *ants, t_graph *graph, char **file)
 	char	*cmd;
 	char	*line;
 
-	ret = 0;
+	ret = 1;
 	cmd = NULL;
+	line = NULL;
 	while (ret >= 0 && (ret = get_next_line(STDIN, &line)) > 0)
 	{
-		if (!*ants && !is_comment(line))
-			ret = get_ants(line, ants);
-		else if (is_command(line))
-			get_command(line, &cmd);
-		else if (!is_comment(line))
-			ret = parse_line(graph, line, &cmd);
-		if (ret >= 0)
-			*file = ft_strappendn(*file, 2, line, "\n");
+		*file = ft_strappendn(*file, 2, line, "\n");
+		if (ret > 0 && !(*line == '#' && *(line + 1) != '#'))
+		{
+			if (*ants > 0 && *line == '#' && line[1] == '#')
+				get_cmd(line, &cmd);
+			else if (*ants == 0)
+				ret = get_ants(line, ants);
+			else
+				ret = parse_line(graph, line, &cmd);
+		}
 		ft_strdel(&line);
 	}
-	if (cmd)
-		ft_strdel(&cmd);
-	if (ret >= 0 && data_missing(graph))
-		return (-1);
-	return (ret);
+	if (ret >= 0)
+		ret = data_missing(graph) ? -1 : ret;
+	return (local_exit(ret, NULL, cmd));
 }
