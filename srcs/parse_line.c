5ddd426b6c502d1b4e24fc29ef6a950e253eff93
bpopov <byurinov1@gmail.com>
2019-01-03 21:31:47 +0100

    [MOD] Command is now checked from a pointer to its last occurence in graph structure

--- srcs/parse_line.c
+++ srcs/parse_line.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/28 13:14:02 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/30 19:49:08 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/01/03 18:07:30 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,28 +29,13 @@ static int		is_room(const char *line)
 	return (ft_count_words(line, WSPCS) == 3);
 }
 
-static int		check_last_command(t_graph *graph)
-{
-	t_cmd *ptr;
-
-	ptr = ft_vector_get(graph->command_list, graph->command_list->size);
-	if (ptr)
-	{
-		if (!ptr->room_name)
-			return (1);
-		else
-			return (0);
-	}
-	return (1);
-}
-
 int		parse_line(t_graph *graph, const char *line)
 {
 	if (line[0] == 'L')
 		return (local_exit(-1, "Line beginning by 'L'."));
 	else if (is_link(line))
 	{
-		if (!check_last_command(graph))
+		if (graph->last_command)
 			return (local_exit(-1, "Command before link."));
 		else if (record_link_if_valid(graph, line) >= 0)
 			return (0);
