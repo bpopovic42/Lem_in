48dc066eca9092730b119f825f461534633b6970
bpopov <byurinov1@gmail.com>
2019-01-03 21:33:47 +0100

    [MOD] Modified how commands are handled

--- srcs/record_room.c
+++ srcs/record_room.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 18:41:26 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/27 13:24:32 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/01/03 21:32:55 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ static t_room	*new_room(char *name, char **cmd, t_pos *coord)
 	return (new_room);
 }
 
-t_room			*record_room(t_graph *graph, char **room_data, char **cmd)
+t_room			*record_room(t_graph *graph, char **room_data)
 {
 	t_pos	coord;
 	t_room	*room;
 
 	coord.x = ft_atoi(room_data[1]);
 	coord.y = ft_atoi(room_data[2]);
-	room = new_room(room_data[0], cmd, &coord);
+	room = new_room(room_data[0], &graph->last_command, &coord);
 	if (room->command)
 	{
 		if (!ft_strcmp(room->command, "##start"))
