4384763c8eb15714b526286b856920fe16f7f31b
bpopov <byurinov1@gmail.com>
2019-01-03 21:36:06 +0100

    [MOD][RM] Deleted cmd structure's specific function as it is no longer used

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/24 01:40:03 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/30 19:56:03 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/01/03 21:20:01 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 
 void		free_room(void *room)
 {
-	if (((t_room*)room)->command != NULL)
-		ft_strdel(&(((t_room*)room)->command));
-	ft_strdel(&(((t_room*)room)->name));
-	if (((t_room*)room)->links)
-		ft_vector_free(((t_room*)room)->links, (void*)(void**)&ft_strdel);
-	((t_room*)room)->pos.x = 0;
-	((t_room*)room)->pos.y = 0;
+	t_room *target;
+
+	target = room;
+	if (target->command != NULL)
+		ft_strdel(&(target->command));
+	ft_strdel(&(target->name));
+	if (target->links != NULL)
+		ft_vector_free(target->links, (void*)(void**)&ft_strdel);
+	target->pos.x = 0;
+	target->pos.y = 0;
 	free(room);
 }
 
@@ -32,34 +35,23 @@ void	init_graph(t_graph *graph)
 	graph->start = NULL;
 	graph->end = NULL;
 	graph->room_list = ft_vector_init(sizeof(char*), 0);
-	graph->command_list = ft_vector_init(sizeof(t_cmd*), 0);
 	graph->rooms = ft_hash_newtable(100);
-}
-
-static void	free_command(t_cmd **command)
-{
-	if (command)
-	{
-		if ((*command)->command)
-			ft_strdel(&((*command)->command));
-		if ((*command)->room_name)
-			ft_strdel(&((*command)->room_name));
-	}
+	graph->last_command = NULL;
 }
 
 void		free_graph(t_graph *graph)
 {
 	if (graph->room_list)
 		ft_vector_free(graph->room_list, (void(*)(void**))&ft_strdel);
-	if (graph->command_list)
-		ft_vector_free(graph->command_list, (void(*)(void**))&free_command);
 	if (graph->rooms)
 		ft_hash_freetable(&graph->rooms, &free_room);
 	graph->nbr_of_rooms = 0;
 	graph->nbr_of_links = 0;
 	graph->room_list = NULL;
-	graph->command_list = NULL;
 	graph->rooms = NULL;
+	if (graph->last_command)
+		ft_strdel(&graph->last_command);
+
 }
 
 void			lemin_perror(const char *msg)
@@ -69,15 +61,4 @@ void			lemin_perror(const char *msg)
 	ft_putstr("ERROR");
 	if (ERR_DBG)
 		ft_printf(": %s{eoc}\n", msg);
-}
-
-char	*get_last_command(t_graph *graph)
-{
-	t_cmd *ptr;
-
-	ptr = ft_vector_get(graph->command_list, graph->command_list->size);
-	if (ptr && ptr->command)
-		return (ptr->command);
-	else
-		return (NULL);
 }
