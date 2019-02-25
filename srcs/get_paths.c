f2249b58141225116a72360ecb22288d1f40a375
Boris POPOVIC <bopopovi@student.42.fr>
2019-02-25 20:29:41 +0100

    [MOD] Now stores rooms directly instead of their names.

--- srcs/get_paths.c
+++ srcs/get_paths.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 15:12:38 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/02/21 18:52:56 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/02/25 20:28:43 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 #include "ft_printf.h"
 
 /* UTILS **********************************************************************/
+
+static void print_room_name(t_room **room)
+{
+	ft_putendl((*room)->name);
+}
 
 static void print_final_paths(t_graph *graph)
 {
@@ -24,17 +29,9 @@ static void print_final_paths(t_graph *graph)
 	while (i < graph->paths->size)
 	{
 		ptr = ft_vector_get(graph->paths, i);
-		ft_dlstiter_data(ptr, (void*)&ft_putendl);
+		ft_dlstiter_data(ptr, (void*)&print_room_name);
 		i++;
 	}
-}
-
-static t_room	*get_last_room_in_path(t_graph *graph, t_dlist *path)
-{
-	char *room_name;
-
-	room_name = path->content;
-	return (ft_hashget_data(graph->rooms, room_name));
 }
 
 static t_room *get_room_from_vector(t_graph *graph, t_vect *v, size_t index)
@@ -49,36 +46,36 @@ static t_room *get_room_from_vector(t_graph *graph, t_vect *v, size_t index)
 
 /* BFS MAIN *******************************************************************/
 
-static void mark_visited(t_graph *graph, t_list **paths)
+static void mark_visited(t_graph *graph, t_list *paths)
 {
 	t_list		*ptr;
 	t_room		*room;
 
-	ptr = *paths;
+	ptr = paths;
 	while (ptr)
 	{
-		room = get_last_room_in_path(graph, *((t_dlist**)ptr->content));
+		room = *((t_room**)(*(t_dlist**)ptr->content)->content);// Here we get ptr->content which is a t_dlist* pointer saved in a void* pointer, therefore a t_dlist** pointer, same applies for ptr->content->content
 		if (ft_strcmp(graph->end->name, room->name))
 			room->visited = 1;
 		ptr = ptr->next;
 	}
 }
 
-static void record_final_path(t_graph *graph, t_dlist **base_path, char *room)
+static void record_final_path(t_graph *graph, t_dlist **base_path, t_room *room)
 {
 	t_dlist *final_path;
 
 	final_path = ft_dlstdup(base_path);
-	ft_dlstpush(&final_path, ft_dlstnew(room, ft_strlen(room) + 1));
+	ft_dlstpush(&final_path, ft_dlstnew(&room, sizeof(room)));
 	ft_vector_append(graph->paths, final_path);
 }
 
-static void append_room_to_path(t_list **paths, t_dlist **base_path, char *room)
+static void append_room_to_path(t_list **paths, t_dlist **base_path, t_room *room)
 {
 	t_dlist *new_path;
 
 	new_path = ft_dlstdup(base_path);
-	ft_dlstpush(&new_path, ft_dlstnew(room, ft_strlen(room) + 1));
+	ft_dlstpush(&new_path, ft_dlstnew(&room, sizeof(room)));
 	ft_lstadd(paths, ft_lstnew(&new_path, sizeof(t_dlist**)));
 }
 
@@ -95,29 +92,29 @@ static void get_next_rooms(t_graph *graph, t_list **paths)
 	while (path_ptr)
 	{
 		i = 0;
-		last_room = get_last_room_in_path(graph, *((t_dlist**)path_ptr->content));
+		last_room = *((t_room**)(*(t_dlist**)path_ptr->content)->content);
 		while (i < last_room->links->size)
 		{
 			next = get_room_from_vector(graph, last_room->links, i);
 			if (next->command && !ft_strcmp("##end", next->command))
 			{
 				ft_putendl("Found end !!!!!!!!!!!!");
-				record_final_path(graph, path_ptr->content, next->name);
+				record_final_path(graph, path_ptr->content, next);
 			}
 			else if (next->visited == 0)
-				append_room_to_path(paths, path_ptr->content, next->name);
+				append_room_to_path(paths, path_ptr->content, next);
 			i++;
 		}
 		path_ptr = path_ptr->next;
 	}
 }
 
-static void	bfs(t_graph *graph, t_list **paths, int depth)
+static void	bfs(t_graph *graph, t_list *paths, int depth)
 {
 	while (graph->paths->size == 0)
 	{
 		mark_visited(graph, paths);
-		get_next_rooms(graph, paths);
+		get_next_rooms(graph, &paths);
 		depth++;
 	}
 	print_final_paths(graph);
@@ -129,8 +126,9 @@ int		get_paths(t_graph *graph)
 	t_dlist *start;
 
 	paths = NULL;
-	start = ft_dlstnew(graph->start->name, ft_strlen(graph->start->name) + 1);
-	ft_lstadd(&paths, ft_lstnew(&start, sizeof(t_dlist**)));
-	bfs(graph, &paths, 0);
+	start = NULL;
+	start = ft_dlstnew(&graph->start, sizeof(graph->start)); // content (&graph->start) will be dereferenced and copied, therefore the pointer 'graph->start' will be stored in new->content
+	paths = ft_lstnew(&start, sizeof(start)); // Same here
+	bfs(graph, paths, 0);
 	return (0);
 }
