8bb94cc69beadeffec8165815a236d23b0015e0b
bpopov <byurinov1@gmail.com>
2019-03-01 01:44:29 +0100

    [ADD] Debugging function to print all paths in a given set.

--- srcs/algo/tmp_debug_tools.c
+++ srcs/algo/tmp_debug_tools.c
@@ -6,11 +6,29 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 17:01:50 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/02/27 17:15:22 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/03/01 01:11:46 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+#include "ft_printf.h"
+
+void	print_set(t_set *set)
+{
+	t_list *ptr;
+	int i;
+
+	ptr = set->paths;
+	i = 0;
+	while (ptr)
+	{
+		ft_printf("Path %d :\n", i);
+		print_path(*((t_path**)ptr->content));
+		ft_putchar('\n');
+		ptr = ptr->next;
+		i++;
+	}
+}
 
 void	print_path(t_path *path)
 {
