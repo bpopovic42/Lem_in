9397ff1e08efe994bcefc816f9f29699ca4c70d1
Boris POPOVIC <bopopovi@student.42.fr>
2019-03-04 19:08:43 +0100

    [ADD] Debugging function to print ending paths sets.

--- srcs/algo/tmp_debug_tools.c
+++ srcs/algo/tmp_debug_tools.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 17:01:50 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/03/01 01:11:46 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/03/04 19:03:59 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,11 +36,29 @@ void	print_path(t_path *path)
 
 	if (path)
 	{
+		ft_printf("ID : %d\n", path->id);
 		ptr = path->head;
 		while (ptr)
 		{
 			ft_putendl((*((t_room**)ptr->content))->name);
 			ptr = ptr->next;
 		}
+	}
+}
+
+void print_end_paths(t_list *end_paths)
+{
+	t_list *ptr;
+	int		i;
+
+	ptr = end_paths;
+	i = 0;
+	while (ptr)
+	{
+		ft_printf("\n###### SET %d #####\n\n", i);
+		print_set(*(t_set**)ptr->content);
+		ft_printf("###### SET END ######\n");
+		ptr = ptr->next;
+		i++;
 	}
 }
