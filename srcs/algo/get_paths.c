b946909dd142180f44f89a958972679a9b1d381a
bpopov <byurinov1@gmail.com>
2019-03-04 11:59:21 +0100

    [WIP] Refactoring code.

--- srcs/algo/get_paths.c
+++ srcs/algo/get_paths.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 15:12:38 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/03/01 03:03:42 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/03/01 16:39:25 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,11 @@ static int	init_first_path(t_graph *graph, t_set **all_paths)
 	return (0);
 }
 
+static void		set_ptr_null(t_room **room, size_t data_size)
+{
+	ft_bzero(room, data_size);
+}
+
 static int	room_has_id(t_room *room, int id)
 {
 	t_list *id_ptr;
@@ -44,25 +49,25 @@ static int	room_has_id(t_room *room, int id)
 	return (0);
 }
 
-static t_vect *get_next_rooms_for_path(t_path **path)
+static t_vect *get_next_rooms_for_path(t_path *path)
 {
-	// ADD IS STUCK AND IS END SOMEWHERE IN THIS FUNCTION ?
 	t_vect	*next_rooms;
 	t_room	**last_room;
 	t_room	*room_ptr;
-	size_t		i;
+	size_t	i;
 
 	next_rooms = ft_vector_init(sizeof(t_room*), 0);
-	last_room = (*path)->head->content;
+	last_room = path->head->content;
 	i = 0;
 	while (i < (*last_room)->links->size)
 	{
 		room_ptr = ft_vector_get((*last_room)->links, i);
-		if (!room_ptr->command || ft_strcmp("##start", room_ptr->command))
+		if (!room_is_start(room_ptr) && !room_has_id(room_ptr, path->id))
 		{
-			if (!room_has_id(room_ptr, (*path)->id))
+			if (ft_vector_append(next_rooms, room_ptr) < 0)
 			{
-				ft_vector_append(next_rooms, room_ptr); // Need protection !
+				ft_vector_free(next_rooms, (void*)&set_ptr_null);
+				return (NULL);
 			}
 		}
 		i++;
@@ -70,7 +75,7 @@ static t_vect *get_next_rooms_for_path(t_path **path)
 	return (next_rooms);
 }
 
-static int	append_next_rooms(t_set *all_paths, t_path **path, t_vect *next_rooms)
+static int	append_next_rooms(t_set *all_paths, t_path *path, t_vect *next_rooms)
 {
 	size_t i;
 	t_path *new_path;
@@ -81,13 +86,11 @@ static int	append_next_rooms(t_set *all_paths, t_path **path, t_vect *next_rooms
 	while (i < next_rooms->size)
 	{
 		room_ptr = ft_vector_get(next_rooms, i);
-		if (next_rooms->size - i == 1)
-		{
-			path_add_room(*path, room_ptr);
-		}
+		if (i == next_rooms->size - 1)
+			path_add_room(path, room_ptr);
 		else
 		{
-			if (!(new_path = path_duplicate(*path, all_paths->nbr_of_paths + 1)))
+			if (!(new_path = path_duplicate(path, all_paths->nbr_of_paths + 1)))
 				return (-1);
 			path_mark_visited(new_path);
 			path_add_room(new_path, room_ptr);
@@ -98,47 +101,44 @@ static int	append_next_rooms(t_set *all_paths, t_path **path, t_vect *next_rooms
 	return (0);
 }
 
-static void tmp_erase_room_ptr(t_room **room)
-{
-	ft_bzero(room, sizeof(*room));
-}
-
-static void	get_next_depth(t_set *all_paths)
+static int	get_next_depth(t_set *all_paths)
 {
 	t_list *paths_ptr;
+	t_path *path;
 	t_vect *next_rooms;
 
 	paths_ptr = all_paths->paths;
 	while (paths_ptr)
 	{
-		if (!path_is_stuck(*(t_path**)paths_ptr->content) && !path_has_end(*(t_path**)paths_ptr->content))
+		path = *(t_path**)paths_ptr->content;
+		if (!path->is_stuck && !path->has_end)
 		{
-			next_rooms = get_next_rooms_for_path(paths_ptr->content);
+			if (!(next_rooms = get_next_rooms_for_path(path)))
+				return (-1);
 			if (next_rooms->size > 0)
-			{
-				append_next_rooms(all_paths, paths_ptr->content, next_rooms);
-			}
+				append_next_rooms(all_paths, path, next_rooms);
 			else
-				path_set_stuck(*(t_path**)paths_ptr->content);
-			ft_vector_free(next_rooms, (void*)&tmp_erase_room_ptr);// Get right free function
+				path->is_stuck = 1;
+			ft_vector_free(next_rooms, (void*)&set_ptr_null);
 		}
 		paths_ptr = paths_ptr->next;
 	}
+	return (0);
 }
 
-static void	bfs(t_set *all_paths)
+static int	bfs(t_set *all_paths)
 {
 	int depth;
 
 	depth = 0;
-	while (/*paths_to_end->nbr_of_paths*/depth < 4)
+	while (/*paths_to_end->nbr_of_paths*/depth < 10)
 	{
-		get_next_depth(all_paths);
-		//get_next_rooms(&paths, final_paths);
+		if (get_next_depth(all_paths) < 0)
+			return (-1);
 		depth++;
 	}
 	print_set(all_paths);
-	//print_final_paths(final_paths);
+	return (depth);
 }
 
 int		get_paths(t_graph *graph)
@@ -154,7 +154,11 @@ int		get_paths(t_graph *graph)
 	}
 	else
 	{
-		bfs(all_paths);
+		if (bfs(all_paths) < 0)
+		{
+			set_free(all_paths);
+			return (-1);
+		}
 	}
 	set_free(all_paths);
 	return (0);
