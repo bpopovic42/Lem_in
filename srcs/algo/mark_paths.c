73bf0b0fe40a0dac6f775c5057cd7ab79644ca6a
Boris POPOVIC <bopopovi@student.42.fr>
2019-03-20 20:00:56 +0100

    [WIP] Refactoring 'mark_paths' function

--- srcs/algo/mark_paths.c
+++ srcs/algo/mark_paths.c
@@ -6,19 +6,19 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/19 03:00:14 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/03/20 19:23:40 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/03/20 20:00:00 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-/*static void	erase_ptr(void **ptr)
+static void	erase_ptr(void ***ptr)
 {
-	t_room **room_ptr;
+	t_room ***room_ptr;
 
-	room_ptr = (t_room**)ptr;
-	free(*ptr);
-}*/
+	room_ptr = (t_room***)ptr;
+	free(**room_ptr);
+}
 
 static void	mark_next_rooms(t_room *room, t_vect *next_rooms, int is_start)
 {
@@ -50,46 +50,60 @@ static int	mark_rooms(t_vect *rooms, int is_start)
 	t_room	*ptr;
 	t_vect	*next_rooms;
 
-	i = 0;
 	while (rooms->size)
 	{
-		if (!(next_rooms = ft_vector_init(sizeof(t_room*), 0)))
-			return (-1);
-		while (i < rooms->size)
+		if ((next_rooms = ft_vector_init(sizeof(t_room*), 0)))
 		{
-			ptr = ft_vector_get(rooms, i);
-			mark_next_rooms(ptr, next_rooms, is_start);
-			i++;
+			i = 0;
+			while (i < rooms->size)
+			{
+				ptr = ft_vector_get(rooms, i);
+				mark_next_rooms(ptr, next_rooms, is_start);
+				i++;
+			}
+			ft_vector_free(rooms, (void*)&erase_ptr);
+			rooms = next_rooms;
 		}
-		i = 0;
-		//ft_vector_free(rooms, (void*)&erase_ptr);
-		rooms = next_rooms;
+		else
+			return (-1);
 	}
 	return (0);
 }
 
-int			mark_paths(t_list *source, int is_start)
+static void mark_path_head(t_path *path, int path_id, int is_start)
+{
+	if (is_start)
+	path->head->start_id = path_id;
+	else
+		path->head->end_id = path_id;
+	path->path_id = path_id;
+}
+
+int			mark_paths(t_list *paths_list, int is_start)
 {
 	size_t	path_id;
+	t_node	*list_ptr;
 	t_vect	*rooms;
-	t_node	*ptr;
 
 	path_id = 0;
-	ptr = source->head;
+	list_ptr = paths_list->head;
 	if (!(rooms = ft_vector_init(sizeof(t_room*), 0)))
 		return (-1);
-	while (ptr)
+	while (list_ptr)
 	{
-		if (is_start)
-			((t_room*)(*(t_path**)ptr->data)->head)->start_id = path_id;
+		if (ft_vector_append(rooms, (*(t_path**)list_ptr->data)->head) >= 0)
+		{
+			mark_path_head(*(t_path**)list_ptr->data, path_id, is_start);
+			list_ptr = list_ptr->next;
+			path_id++;
+		}
 		else
-			((t_room*)(*(t_path**)ptr->data)->head)->end_id = path_id;
-		if (ft_vector_append(rooms, (*(t_path**)ptr->data)->head) < 0)
+		{
+			ft_vector_free(rooms, (void*)&erase_ptr);
 			return (-1);
-		(*(t_path**)ptr->data)->path_id = path_id;
-		path_id++;
-		ptr = ptr->next;
+		}
 	}
-	mark_rooms(rooms, is_start);
+	if (mark_rooms(rooms, is_start) < 0)
+		return (-1);
 	return (0);
 }
