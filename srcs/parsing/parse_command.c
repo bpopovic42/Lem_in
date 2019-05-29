1b578588d633375c7109d5ba3c669fea2fd4208f
Boris POPOVIC <bopopovi@student.42.fr>
2019-05-29 20:48:29 +0200

    [FIX] Bad use of 'ft_strnstr' function, length of needle was passed instead of haystack.

--- srcs/parsing/parse_command.c
+++ srcs/parsing/parse_command.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 21:52:17 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/05/29 19:39:24 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/05/29 20:47:23 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static int	record_command(t_graph *graph, char **cmd_list, const char *cmd)
 
 static int	command_already_exist(const char *cmd, char **cmd_list)
 {
-	return (*cmd_list && ft_strnstr(*cmd_list, cmd, ft_strlen(cmd)));
+	return (*cmd_list && ft_strnstr(*cmd_list, cmd, ft_strlen(*cmd_list)));
 }
 
 int			parse_command(t_graph *graph, const char *cmd, char **cmd_list)
