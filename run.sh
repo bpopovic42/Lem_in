b6092149b6ed56d0d2e27d33f2941c73ae2f948f
bpopov <byurinov1@gmail.com>
2019-06-14 21:40:40 +0200

    [MOD] Use 'make' local alias to build the exe before running it.

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+shopt -s expand_aliases
+source $HOME/.usr_cfg/mixins/aliases
+
 TARGET="lem-in"
 OPTIONS="$1"
 
 if [[ $OPTIONS != "" ]]; then
+	mkfsn
 	ASAN_OPTIONS=detect_leaks=1 ./$TARGET < $OPTIONS
 else
 	exit 1
