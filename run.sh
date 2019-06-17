7160975ba810597be37477385d91fe6ef1e906d9
bpopov <byurinov1@gmail.com>
2019-06-17 15:45:31 +0200

    [FIX] Running script shouldn't run the executable if compilation failed.

--- run.sh
+++ run.sh
@@ -7,8 +7,7 @@ TARGET="lem-in"
 OPTIONS="$1"
 
 if [[ $OPTIONS != "" ]]; then
-	mkfsn
-	ASAN_OPTIONS=detect_leaks=1 ./$TARGET < $OPTIONS
+	mkfsn && ASAN_OPTIONS=detect_leaks=1 ./$TARGET < $OPTIONS
 else
 	exit 1
 fi
