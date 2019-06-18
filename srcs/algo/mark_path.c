7e15d041c22561c3dc3a37169eeab0a7a73f430e
Boris POPOVIC <bopopovi@student.42.fr>
2019-06-18 17:46:41 +0200

    [NORM]

--- srcs/algo/mark_path.c
+++ srcs/algo/mark_path.c
@@ -6,14 +6,14 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 20:52:23 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/06/18 17:35:49 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/06/18 17:46:30 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include "ft_printf.h"
 
-static void	mark_next_room(t_room **from, t_room **next)
+static void		mark_next_room(t_room **from, t_room **next)
 {
 	print_dbg(0, "\t[Marking] %s->%s\n", (*from)->name, (*next)->name);
 	(*from)->to = (*next);
@@ -23,41 +23,25 @@ static void	mark_next_room(t_room **from, t_room **next)
 	{
 		(*next)->start_distance = (*from)->start_distance + 1;
 		(*next)->from = (*from);
-		(*from) = (*next); //
+		(*from) = (*next);
 	}
 }
 
 static int		link_is_shorter(t_room *src, t_room *link, t_room *shortest)
 {
-	//ft_printf("for room %s link %s is ", src->name, link->name);
 	if (link == src->from || room_is_start(link) || link->blocked)
-	{
-		/*if (link == src->from)
-			ft_putendl("previous");
-		else if (room_is_start(link))
-			ft_putendl("start");
-		else if (link->blocked)
-			ft_putendl("blocked");*/
 		return (0);
-	}
 	else if (link->end_distance >= 0)
 	{
 		if (shortest == NULL || shortest->end_distance > link->end_distance)
-		{
-			//ft_putendl("valid");
 			return (1);
-		}
 		else
-		{
-			//ft_printf("longer than %s\n", shortest->name);
 			return (0);
-		}
 	}
-	//ft_printf("unmarked (%d)\n", link->end_distance);
 	return (0);
 }
 
-static t_room	*get_shortest_link(t_room *src) //DIRTY
+static t_room	*get_shortest_link(t_room *src)
 {
 	t_node	*link_ptr;
 	t_room	*shortest;
@@ -69,12 +53,8 @@ static t_room	*get_shortest_link(t_room *src) //DIRTY
 	while (link_ptr)
 	{
 		link = get_room_from_node(link_ptr);
-		if (room_is_end(link))//WUT
-		{
-			//if (room_is_start(src) && link->blocked)
-			//	return (NULL);
+		if (room_is_end(link))
 			return (link);
-		}
 		else if (link_is_shorter(src, link, shortest))
 			shortest = link;
 		link_ptr = link_ptr->next;
@@ -82,7 +62,7 @@ static t_room	*get_shortest_link(t_room *src) //DIRTY
 	return (shortest);
 }
 
-static void	unmark_path(t_path *path, t_room *last_marked_room)
+static void		unmark_path(t_path *path, t_room *last_marked_room)
 {
 	t_room *ptr;
 	t_room *previous;
@@ -96,29 +76,27 @@ static void	unmark_path(t_path *path, t_room *last_marked_room)
 		ptr->from = NULL;
 		ptr = previous;
 	}
-	path->head->blocked = 1; //So that path is not re-selected for current set
+	path->head->blocked = 1;
 	path->length = -1;
 }
 
-void	mark_path(t_path *path)
+void			mark_path(t_path *path)
 {
 	t_room	*ptr;
 	t_room	*from;
 
 	ptr = NULL;
 	from = path->head;
-	print_dbg(0, "Marking path %s :\n", from->name);
 	from->start_distance = 1;
-	while (!room_is_end(ptr) && (ptr = get_shortest_link(from))) //Might cause some issues
+	print_dbg(0, "Marking path %s :\n", from->name);
+	while (!room_is_end(ptr) && (ptr = get_shortest_link(from)))
 		mark_next_room(&from, &ptr);
-
-	if ((from && room_is_end(ptr)) || (room_is_end(from) && !ptr)) // if end is properly reached or start->end
+	if ((from && room_is_end(ptr)) || (room_is_end(from) && !ptr))
 	{
-		print_dbg(2, "\tPath successfully marked, setting length %d\n",
-			from->start_distance);
+		print_dbg(2, "\tPath marked, length %d\n", from->start_distance);
 		path_set_length(path, from->start_distance);
-		if (!ptr) // if start->end, block it
-			path->head->blocked = 1; //UNSURE
+		if (!ptr)
+			path->head->blocked = 1;
 	}
 	else
 	{
