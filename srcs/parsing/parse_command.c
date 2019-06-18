17a7a1f9fec5e6f1bdee6981d1b8601374864a9e
Boris POPOVIC <bopopovi@student.42.fr>
2019-06-18 20:27:54 +0200

    [NORM]

--- srcs/parsing/parse_command.c
+++ srcs/parsing/parse_command.c
@@ -6,31 +6,31 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 21:52:17 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/05/29 20:47:23 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/06/18 20:27:44 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-static int	record_command(t_graph *graph, char **cmd_list, const char *cmd)
+static int	record_command(t_graph *graph, char **cmds, const char *cmd)
 {
 	if (!(graph->last_command = ft_strdup(cmd)))
 		return (-1);
-	if (!(*cmd_list = ft_strappendn(*cmd_list, 2, cmd, "\n")))
+	if (!(*cmds = ft_strappendn(*cmds, 2, cmd, "\n")))
 		return (-1);
 	return (0);
 }
 
-static int	command_already_exist(const char *cmd, char **cmd_list)
+static int	command_already_exist(const char *cmd, char **cmds)
 {
-	return (*cmd_list && ft_strnstr(*cmd_list, cmd, ft_strlen(*cmd_list)));
+	return (*cmds && ft_strnstr(*cmds, cmd, ft_strlen(*cmds)));
 }
 
-int			parse_command(t_graph *graph, const char *cmd, char **cmd_list)
+int			parse_command(t_graph *graph, const char *cmd, char **cmds)
 {
-	if (graph->last_command || command_already_exist(cmd, cmd_list))
+	if (graph->last_command || command_already_exist(cmd, cmds))
 		return (1);
-	if (record_command(graph, cmd_list, cmd) < 0)
+	if (record_command(graph, cmds, cmd) < 0)
 		return (-1);
 	return (0);
 }
