f136a1a13297bb0bea45e121006bd39030b90228
Boris POPOVIC <bopopovi@student.42.fr>
2019-06-18 20:27:01 +0200

    [NORM]

--- srcs/parsing/parse_line.c
+++ srcs/parsing/parse_line.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/28 13:14:02 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/06/18 16:48:02 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/06/18 20:26:44 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,20 @@ static int	get_ants(const char *input, int *ants)
 	return (0);
 }
 
-int			parse_line(t_graph *g, int *ants, const char *line, char **cmd_list)
+int			parse_line(t_graph *graph, int *ants, const char *line, char **cmds)
 {
 	if (line[0] == 'L')
 		return (1);
 	else if (!line_is_comment(line))
 	{
 		if (line_is_command(line) && *ants > 0)
-			return (parse_command(g, line, cmd_list));
+			return (parse_command(graph, line, cmds));
 		else if (!(*ants))
 			return (get_ants(line, ants));
 		else if (line_is_room(line))
-			return (parse_room(g, line));
-		else if (line_is_link(line) && !g->last_command)
-			return (parse_link(g, line));
+			return (parse_room(graph, line));
+		else if (line_is_link(line) && !graph->last_command)
+			return (parse_link(graph, line));
 		else
 			return (1);
 	}
