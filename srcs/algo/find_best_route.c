6364ef0578c276a2bff8d43559ff22ca6cc08114
Boris POPOVIC <bopopovi@student.42.fr>
2019-06-19 21:27:51 +0200

    [MOD] Refactored, divided code into local sub-functions.

--- srcs/algo/find_best_route.c
+++ srcs/algo/find_best_route.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 20:39:12 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/06/18 20:16:04 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/06/19 17:19:35 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,28 @@ static void	clean_path_lengths(t_path ***path)
 	(**path)->final_length = -1;
 }
 
+static int	set_break_flag(int *break_flag, t_node *node_ptr)
+{
+	if (!node_ptr && !*break_flag)
+	{
+		*break_flag = 1;
+		return (1);
+	}
+	return (0);
+}
+
+static void	mark_first_path(t_graph *graph, t_path *first_path, t_queue *bfs)
+{
+	reweight_graph(graph, bfs);
+	mark_path(first_path);
+}
+
+static void	clean_graph_and_paths(t_graph *graph, t_list *paths)
+{
+	clean_graph(graph);
+	ft_lstiter(paths, (void*)&clean_path_lengths);
+}
+
 int			find_best_route(t_graph *graph, t_route *route, t_queue *bfs)
 {
 	int		break_flag;
@@ -27,24 +49,19 @@ int			find_best_route(t_graph *graph, t_route *route, t_queue *bfs)
 	break_flag = 0;
 	first_path = NULL;
 	node_ptr = route->paths->head;
-	ft_lstiter(route->paths, (void*)&clean_path_lengths);
-	while ((first_path = get_path_from_node(node_ptr)))
+	while (node_ptr)
 	{
 		print_dbg(0, "\t--- NEXT TURN ---\n");
+		clean_graph_and_paths(graph, route->paths);
+		first_path = get_path_from_node(node_ptr);
 		print_dbg(0, "\t(1st path is %s)\n", first_path->head->name);
-		reweight_graph(graph, bfs);
-		mark_path(first_path);
+		mark_first_path(graph, first_path, bfs);
 		mark_next_paths(graph, route->paths, bfs);
 		update_score(graph, route);
-		if (!break_flag || !break_link(first_path))
+		if ((!break_flag || !break_link(first_path)))
 			node_ptr = node_ptr->next;
-		if (!node_ptr && !break_flag)
-		{
-			break_flag = 1;
+		if (set_break_flag(&break_flag, node_ptr))
 			node_ptr = route->paths->head;
-		}
-		clean_graph(graph);
-		ft_lstiter(route->paths, (void*)&clean_path_lengths);
 		print_dbg(1, "", NULL);
 	}
 	return (0);
