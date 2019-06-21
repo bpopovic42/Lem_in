85def2600244036cce08a7d94022d84325eb7fc7
Boris POPOVIC <bopopovi@student.42.fr>
2019-06-21 16:30:56 +0200

    [ADD][MOD] Implemented the 'print_warning' function, renamed 'print_dbg' into 'algo_dbg'.

--- srcs/common/debug.c
+++ srcs/common/debug.c
@@ -6,23 +6,39 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 16:20:25 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/06/13 16:33:04 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/06/21 13:35:16 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include "ft_printf.h"
 
-void	print_dbg(int is_msg_last, const char *fmt_msg, ...)
+void	algo_dbg(int is_msg_last, const char *fmt_msg, ...)
 {
 	va_list ap;
 
-	if (DBG_PRINT == 1)
+	if (ALGO_DBG == 1)
 	{
 		va_start(ap, fmt_msg);
-		ft_vdprintf(1, fmt_msg, ap);
+		ft_vdprintf(2, fmt_msg, ap);
 		if (is_msg_last)
 			ft_putchar('\n');
+		va_end(ap);
+	}
+}
+
+void	print_warning(const char *fmt_msg, ...)
+{
+	va_list ap;
+
+	if (WARN_DBG == 1)
+	{
+		va_start(ap, fmt_msg);
+		ft_dprintf(2, "{yellow}", NULL);
+		ft_putstr("Warning : ");
+		ft_vdprintf(2, fmt_msg, ap);
+		ft_dprintf(2, "{eoc}", NULL);
+		ft_putchar('\n');
 		va_end(ap);
 	}
 }
