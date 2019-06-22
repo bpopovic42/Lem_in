46dafdfc2179fc342c6bd353ecdeee6aefc715d9
Boris POPOVIC <bopopovi@student.42.fr>
2019-06-22 16:56:00 +0200

    [MOD] Moved warnings into their corresponding sub-functions in order to de-clutter 'room_has_conflict()' parent function and to fit the norm.

--- srcs/parsing/room_has_conflict.c
+++ srcs/parsing/room_has_conflict.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 21:05:46 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/06/21 15:14:54 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/06/22 16:54:10 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,32 @@
 
 static int	room_has_duplicated_name(t_room *a, t_room *b)
 {
-	return (!ft_strcmp(a->name, b->name));
+	if (!ft_strcmp(a->name, b->name))
+	{
+		print_warning("Duplicated room name : '%s'", a->name);
+		return (1);
+	}
+	return (0);
 }
 
 static int	room_has_duplicated_command(t_room *a, t_room *b)
 {
-	if (a->command && b->command)
-		return (!ft_strcmp(a->command, b->command));
+	if (a->command && b->command && !ft_strcmp(a->command, b->command))
+	{
+		print_warning("Command redefinition : '%s'", a->command);
+		return (1);
+	}
 	return (0);
 }
 
 static int	room_has_duplicated_coordinates(t_room *a, t_room *b)
 {
-	return (a->pos.x == b->pos.x && a->pos.y == b->pos.y);
+	if (a->pos.x == b->pos.x && a->pos.y == b->pos.y)
+	{
+		print_warning("Duplicated coordinates in room : '%s'", a->name);
+		return (1);
+	}
+	return (0);
 }
 
 int			room_has_conflict(t_graph *graph, t_room *room)
@@ -41,20 +54,11 @@ int			room_has_conflict(t_graph *graph, t_room *room)
 	{
 		ptr = ft_vector_get(graph->room_list, i);
 		if (room_has_duplicated_name(room, ptr))
-		{
-			print_warning("Duplicated room name : '%s'", room->name);
 			return (1);
-		}
 		else if (room_has_duplicated_command(room, ptr))
-		{
-			print_warning("Command redefinition : '%s'", room->command);
 			return (1);
-		}
 		else if (room_has_duplicated_coordinates(room, ptr))
-		{
-			print_warning("Duplicated coordinates in room : '%s'", room->name);
 			return (1);
-		}
 		i++;
 	}
 	return (0);
